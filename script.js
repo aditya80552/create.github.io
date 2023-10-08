7090f09d7ceb918b36edf32213e7026f6c4e4766
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-10-08 16:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,108 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+// Ball object
+class Ball {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.radius = 10;
+    this.dx = 5;
+    this.dy = 5;
+  }
 
-const paddle1 = {
-  x: 10,
-  y: 100,
-  width: 10,
-  height: 50
-};
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
 
-const paddle2 = {
-  x: 480,
-  y: 100,
-  width: 10,
-  height: 50
-};
+    // Check if the ball has hit the top or bottom of the screen
+    if (this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
+      this.dy = -this.dy;
+    }
 
-const ball = {
-  x: 250,
-  y: 150,
-  radius: 5,
-  dx: 5,
-  dy: -5
-};
+    // Check if the ball has gone past a paddle
+    if (this.x < 0 || this.x > canvas.width) {
+      // Game over
+    }
+  }
 
-let score1 = 0;
-let score2 = 0;
-
-let gameOver = false;
-
-let fullScreen = false;
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
-  ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-
-  ctx.fillStyle = "red";
-  ctx.fillRect(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2);
-
-  // Display the score
-  ctx.fillStyle = "white";
-  ctx.font = "20px Arial";
-  ctx.fillText(score1 + "-" + score2, 10, 30);
-
-  // Display the game over message
-  if (gameOver) {
-    ctx.fillStyle = "white";
-    ctx.font = "40px Arial";
-    ctx.fillText("Game over!", 100, 100);
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
   }
 }
 
-function update() {
-  // Move the ball
-  ball.x += ball.dx;
-  ball.y += ball.dy;
+// Paddle object
+class Paddle {
+  constructor(canvas, side) {
+    this.canvas = canvas;
+    this.side = side;
+    this.x = side === "left" ? canvas.width / 10 : canvas.width - canvas.width / 10;
+    this.y = canvas.height / 2;
+    this.width = 10;
+    this.height = 50;
+  }
 
-  // Check for collisions with the paddles
+  update(ball) {
+    // Move the paddle to intercept the ball
+    this.y = ball.y - this.height / 2;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+  // Start the game
+  gameloop();
+});
+
+// AI object
+class AI {
+  constructor(paddle) {
+    this.paddle = paddle;
+  }
+
+  update(ball) {
+    // Move the paddle to intercept the ball
+    this.paddle.y = ball.y - this.paddle.height / 2;
+
+    // Add additional code to make the AI opponent more intelligent
+  }
+}
+
+// Power-ups
+const powerUps = {
+  speedUp: {
+    name: "Speed Up",
+    effect: (player) => {
+      player.speed *= 1.5;
+    },
+  },
+  shrinkBall: {
+    name: "Shrink Ball",
+    effect: (ball) => {
+      ball.radius *= 0.5;
+    },
+  },
+  freezeOpponent: {
+    name: "Freeze Opponent",
+    effect: (opponent) => {
+      opponent.isFrozen = true;
+    },
+  },
+};
+
+// Main game loop
+function gameloop() {
+  // Update the ball
+  ball.update();
+
+  // Update the paddles
+  paddle1.update(ball);
+  paddle2.update(ball);
+
+  // Check for collisions between the ball and the paddles
   if (ball.x - ball.radius < paddle1.x + paddle1.width && ball.y - ball.radius < paddle1.y + paddle1.height && ball.y + ball.radius > paddle1.y) {
     ball.dx = -ball.dx;
   }
@@ -66,58 +111,74 @@ function update() {
     ball.dx = -ball.dx;
   }
 
-  // Check if the ball has hit the top or bottom of the screen
-  if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
-    ball.dy = -ball.dy;
+  // Apply power-ups
+  if (player1.hasPowerUp) {
+    powerUps[player1.powerUp].effect(player1);
+    player1.hasPowerUp = false;
   }
 
-  // Check if the ball has gone past a paddle
-  if (ball.x < 0) {
-    score2++;
-    if (score2 === 11) {
-      gameOver = true;
-    }
-  } else if (ball.x > canvas.width) {
-    score1++;
-    if (score1 === 11) {
-      gameOver = true;
-    }
+  if (player2.hasPowerUp) {
+    powerUps[player2.powerUp].effect(player2);
+    player2.hasPowerUp = false;
   }
-}
 
-function gameloop() {
-  update();
-  draw();
-
-  if (!gameOver) {
-    requestAnimationFrame(gameloop);
-  }
-}
-
-// Start the game
-gameloop();
-
-// Add event listeners to the paddles
-document.addEventListener("keydown", function (e) {
-  if (e.keyCode === 38) {
-    paddle1.y -= 10;
-  } else if (e.keyCode === 40) {
-    paddle1.y += 10;
-  } else if (e.keyCode === 87) {
-    paddle2.y -= 10;
-  } else if (e.keyCode === 83) {
-    paddle2.y += 10;
-  }
+  //
+// Multiplayer mode
+const multiplayerButton = document.querySelector("#start-multiplayer");
+multiplayerButton.addEventListener("click", () => {
+  // Start a multiplayer match
+  // You could use WebSockets or another technology to implement multiplayer mode
 });
 
-// Add an event listener to the full screen button
-document.getElementById("full-screen-button").addEventListener("click", function () {
-  if (fullScreen) {
-    document.exitFullscreen();
-  } else {
-    canvas.requestFullscreen();
-  }
+// Leaderboard
+const leaderboard = document.querySelector("#leaderboard");
+// Update the leaderboard with the latest scores
+// You could store the player's scores in a database and display the top scores on the leaderboard
+// You could also implement a system for players to challenge each other to matches
 
-  fullScreen = !fullScreen;
+// Full screen mode
+const fullScreenButton = document.querySelector("#full-screen-button");
+fullScreenButton.addEventListener("click", () => {
+  // Toggle full screen mode
 });
 
+// Replay button
+const replayButton = document.querySelector("#replay-button");
+replayButton.addEventListener("click", () => {
+  // Restart the game
+});
+
+// Additional features
+// You could add other features to your game, such as:
+// - Different paddle designs
+// - Different ball designs
+// - Different background designs
+// - Sound effects
+// - Music
+
+// Once you have finished adding all of the features that you want, you can test the game to make sure that it is balanced and fun to play. You may need to adjust some of the game settings, such as the speed of the ball or the difficulty of the AI opponent, to make the game challenging but fair.
+
+// Once you are satisfied with the game, you can release it to the public so that other people can enjoy it.
+
+// Here are some additional ideas for features that you could add to your game:
+// - A training mode where the player can practice against a computer opponent
+// - A tournament mode where the player can compete against other players for a high score
+// - A customization mode where the player can change the appearance of their paddle and ball
+// - A power-up shop where the player can purchase power-ups with coins that they earn by playing the game
+
+// I hope this helps!
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+  // Check if the player has selected a difficulty level
+  const difficulty = document.querySelector("#difficulty").value;
+  if (difficulty === "") {
+    // Display an error message to the player
+    return;
+  }
+
+  // Display a message to the player letting them know that the game is about to start
+  alert("The game is about to start!");
+
+  // Start the game
+  gameloop();
+});
